e017f793aa0b4f19aae845497b4e49f5c19424fa
crawford <crawford.collins@gmail.com>
2019-02-11 17:11:38 -0600

    successfully uploaded my package to test.pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Package setup/installation and metadata for lambdata
+
+import setuptools
+
+REQUIRED = ["pandas"]
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata_crawftv",
+    version="0.0.1",
+    author="crawftv",
+    description="Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/crawftv/lambdata",
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
