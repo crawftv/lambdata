c295640ec86eda200783078171747e6879b32bcc
Mark Oliver <invegat@users.noreply.github.com>
2019-02-14 12:02:48 -0500

    reworte function

--- lambdata_crawftv/top_x.py
+++ lambdata_crawftv/top_x.py
@@ -1,14 +1,10 @@
 import pandas as pd
+import numpy as np
 
+"""return the top_x_values most common values of original_df_column of df in order"""
+def top_x(df, original_df_column, top_x_values) -> list: 
+    return list(df[original_df_column].value_counts().head(top_x_values).index)
 
-def top_x(df, new_column, original_df_column, top_x_values):
-    top_x_list = list(
-        df["original_df_column"].value_counts().head(top_x_values).index)
-    df[new_column] = df[original_df_column].apply(lambda x: x in top_x_list)
-    return df[new_column]
+df = pd.DataFrame(np.matrix([1,1,1,1,2,2,3,3,3,4,5,5,5,5,5]).T, columns=['n'])
 
-# top_installers =  x["installer"].value_counts().head(100).index
-# top_installers = list(top_installers)
-# top_installers.remove('0')
-# top_installers
-# x["top_installer"] = x["installer"].apply(lambda x : x in top_installers)
+assert top_x(df,'n',3) == [5, 1, 3]
