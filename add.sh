edd3fd7e6c4bebef0cbc1f5dce74f3ab3e3520ec
Crawford <crawford.collins@gmail.com>
2019-02-15 01:21:48 -0600

    m

--- add.sh
+++ add.sh
@@ -1,5 +1,5 @@
 git add setup.py lambdata_crawftv/ClassificationVisualization.py \
-    lambdata_crawftv/TopX.py lambdata_crawftv/test.py add.sh;
+    lambdata_crawftv/TopX.py lambdata_crawftv/test.py add.sh .gitignore;
 read -p "enter your commit message: " m;
 git status;
 git commit -mm;
